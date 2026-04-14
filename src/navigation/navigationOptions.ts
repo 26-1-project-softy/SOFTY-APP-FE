97386e0b737fbe74ada32c986be70e89b1a0d264
cjiyun <chlwldbs78@gmail.com>
2026-04-15 03:43:59 +0900

    style: 스택 기본 배경색 수정

--- src/navigation/navigationOptions.ts
+++ src/navigation/navigationOptions.ts
@@ -1,9 +1,12 @@
 import type { NativeStackNavigationOptions } from '@react-navigation/native-stack';
+import { theme } from '@/styles/theme';
+
+const DEFAULT_BG_COLOR = theme.colors.background.bg2;
 
 export const defaultStackScreenOptions: NativeStackNavigationOptions = {
   headerShown: false,
   animation: 'slide_from_right',
   contentStyle: {
-    backgroundColor: 'transparent',
+    backgroundColor: DEFAULT_BG_COLOR,
   },
 };
