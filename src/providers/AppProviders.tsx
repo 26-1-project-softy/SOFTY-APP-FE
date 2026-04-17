07a6d4afbc00cfd88d3d1f975fd692a11a2eda76
cjiyun <chlwldbs78@gmail.com>
2026-04-18 04:00:12 +0900

    feat: 전역 StatusBar 스타일 적용

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -5,13 +5,16 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/providers/ThemeProvider';
 import { Toast } from '@/components/common/Toast';
 import { UnauthorizedSessionSync } from '@/providers/UnauthorizedSessionSync';
+import { StatusBar } from 'react-native';
 import { queryClient } from '@/providers/queryClient';
+import { theme } from '@/styles/theme';
 
 export const AppProviders = ({ children }: PropsWithChildren) => {
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
+          <StatusBar barStyle="dark-content" backgroundColor={theme.colors.background.bg1} />
           <NavigationContainer>{children}</NavigationContainer>
           <Toast />
           <UnauthorizedSessionSync />
