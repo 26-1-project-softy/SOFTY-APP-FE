6b817a5c904ac4be6ad64869aac643d50c6eb315
cjiyun <chlwldbs78@gmail.com>
2026-05-03 04:05:05 +0900

    feat: 문의함 새 문의 작성 진입 버튼 추가

--- src/screens/ThreadListScreen.tsx
+++ src/screens/ThreadListScreen.tsx
@@ -1,19 +1,25 @@
 import styled from '@emotion/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Header } from '@/components/common/Header';
-import { IcSettings } from '@/assets/icons';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { MainStackParamList } from '@/types/navigation';
+import { Header } from '@/components/common/Header';
+import { IconButton } from '@/components/common/IconButton';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { MainStackParamList } from '@/types/navigation';
+import { IcSettings, IcPencil } from '@/assets/icons';
 import { MAIN_ROUTES } from '@/navigation/routes';
 
 type ThreadListNavigationProp = NativeStackNavigationProp<MainStackParamList>;
 
 export const ThreadListScreen = () => {
+  const insets = useSafeAreaInsets();
   const navigation = useNavigation<ThreadListNavigationProp>();
 
   const handlePressSettings = () => {
     navigation.navigate(MAIN_ROUTES.SETTINGS);
+  };
+
+  const handlePressCreateInquiry = () => {
+    navigation.navigate(MAIN_ROUTES.NEW_INQUIRY);
   };
 
   return (
@@ -24,22 +30,48 @@ export const ThreadListScreen = () => {
         onActionPress={handlePressSettings}
         actionAccessibilityLabel="설정 열기"
       />
-      <ContentContainer>
+
+      <ThreadListContentContainer>
         <ScreenTitle>{`문의함 화면`}</ScreenTitle>
-      </ContentContainer>
+      </ThreadListContentContainer>
+
+      <FABWrapper style={{ bottom: insets.bottom + 16 }}>
+        <IconButton
+          icon={IcPencil}
+          variant="primary"
+          buttonSize={48}
+          onPress={handlePressCreateInquiry}
+          accessibilityLabel="새 문의 작성"
+        />
+      </FABWrapper>
     </ThreadListScreenContainer>
   );
 };
 
-const ThreadListScreenContainer = styled(SafeAreaView)(({ theme }) => ({
+const ThreadListScreenContainer = styled(SafeAreaView)({
   flex: 1,
-}));
+});
 
-const ContentContainer = styled.View({
+const ThreadListContentContainer = styled.View({
   flex: 1,
 });
 
 const ScreenTitle = styled.Text(({ theme }) => ({
   ...theme.fonts.title3,
   color: theme.colors.text.text1,
+}));
+
+const FABWrapper = styled.View(({ theme }) => ({
+  position: 'absolute',
+  right: 20,
+  borderRadius: 999,
+
+  // 그림자 (iOS)
+  shadowColor: theme.colors.text.text1,
+  shadowOpacity: 0.2,
+  shadowRadius: 6,
+  shadowOffset: { width: 0, height: 3 },
+
+  // 그림자 (Android)
+  elevation: 5,
 }));
